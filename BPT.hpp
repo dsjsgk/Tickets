bc409c2851551034561ecdd86fdcaaa52511fd40
dsjsgk <79463184+dsjsgk@users.noreply.github.com>
2021-06-11 02:40:33 +0800

    Update BPT.hpp

--- BPT.hpp
+++ BPT.hpp
@@ -55,7 +55,7 @@ public:
     void clear(){top = 0;}
 };
 
-template <class Key,class Data,int Cap1 = 379,int Cap2 = 50000>
+template <class Key,class Data,int Cap1 = 379,int Cap2 = 60000>
 class BPT{
 private:
     /**非叶结点最大儿子数M，叶结点最大数据量L**/
